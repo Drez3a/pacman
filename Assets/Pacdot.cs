ef859c76736a73b4f273a4dc1980430e27a4ae07
Karla Falcao <karlapsfalcao@gmail.com>
2018-11-21 11:50:31 -0800

    pacman project in unity

---
+++ Assets/Pacdot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacdot : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D co) {
+         if (co.name == "pacman")
+            Destroy(gameObject);
+    }
+
+    // Note: 
+    // if we wanted to implement a highscore 
+    // in our game, then this would be the place to increase it.
+}
